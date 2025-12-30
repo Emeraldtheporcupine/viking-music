b6786406ff08f40dcd5db0d9cede4ea3f1cc738f
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2025-12-29 20:57:49 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -34,3 +34,4 @@ let mySprite2: Sprite = null
 let mySprite: Sprite = null
 Start()
 let mySprite20251229T161353318Z = sprites.create(assets.image`GameOver`, SpriteKind.Player)
+mySprite20251229T161353318Z.setPosition(80, 60)
